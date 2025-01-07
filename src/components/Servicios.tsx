9a9ec37bf5ac0e2471928a26326ff898e382d21d
Enzo Figlioli <enzofiglioli.p@gmail.com>
2025-01-07 16:16:19 -0300

    A ver su funciona

--- src/components/Servicios.tsx
+++ src/components/Servicios.tsx
@@ -15,23 +15,17 @@ const Servicios = () => {
 
   return (
     <main id="services" className="secciones Servicio p-8 text-white min-h-screen">
-      {/* Título */}
       <Title title={'¿Que es lo que hago?'} />
-
-      {/* Contenedor de servicios */}
       <div className="service-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 mt-8">
         {cards.map((c, index) => (
           <div
             className="card dark:bg-stone-950 bg-gray-50 p-6 rounded-lg shadow-md hover:shadow-lg dark:hover:bg-yellow-500 dark:hover:text-black hover:bg-yellow-500 transition-all h-50"
             key={index}
           >
-            {/* Ícono */}
             <div className=" dark:text-white mb-4 text-gray-400">
               <i className={c.ico}></i>
             </div>
-            {/* Título */}
             <h3 className="text-black dark:text-white font-semibold mb-2">{c.title}</h3>
-            {/* Descripción */}
             <p className="text-black dark:text-white">{c.p}</p>
           </div>
         ))}
