dfe4b945133c7c78d21a32ec0e48fef8d6a8dff4
Enzo Figlioli <enzofiglioli.p@gmail.com>
2026-08-17 10:16:23 -0300

    fixing changing cv

--- src/features/hero/HeroButtons.tsx
+++ src/features/hero/HeroButtons.tsx
@@ -4,8 +4,22 @@ import { hero } from "./hero.data";
 export const HeroButtons = () => {
   return (
     <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-      <Button name={hero.primaryButton.label} color="blue" padding="3" icon="ArrowRight" />
-      <Button name={hero.secondaryButton.label} padding="3" icon="Mail" color="black" />
+      <a href="#projects">
+        <Button
+          name={hero.primaryButton.label}
+          color="blue"
+          padding="3"
+          icon="ArrowRight"
+        />
+      </a>
+      <a href="#contact">
+        <Button
+          name={hero.secondaryButton.label}
+          padding="3"
+          icon="Mail"
+          color="black"
+        />
+      </a>
     </div>
   );
 };
